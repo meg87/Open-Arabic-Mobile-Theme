5fb24b57591d9e69f42c4c37724193ef3e3967f7
MEG <meginccsi@hotmail.com>
2010-05-20 13:38:23 +0300

    changes on home page include entry-title and read more

--- index.php
+++ index.php
@@ -8,26 +8,17 @@
 				if(is_single())
 					the_content('اقرأ المزيد');
 				else
-				{
-					the_excerpt();?>
-					<a href="<?php the_permalink(); ?>">اكمل القراءة ..</a><?php
-				}
-				?>
+					the_excerpt();
 
-				<?php
 				if(is_single())
 				{?>
-					<p class="metadata"><b>* كتب بواسطة</b> <?php echo get_the_author(); ?>, <b>بتاريخ</b> <?php echo the_date('j F Y'); ?><br />
-					<b> تحت الاقسام:</b> <?php the_category(', ') ?></p> <!--| <?php edit_post_link('تحرير','',' |'); ?> <?php comments_popup_link('لا تعليق', 'تعليق واحد', '% تعليق'); ?>-->
+					<p class="metadata"><b>كتب بواسطة</b> <?php echo get_the_author(); ?>, <b>بتاريخ</b> <?php echo the_date('j F Y'); ?><br />
+					<b> تحت الاقسام:</b> <?php the_category(', ') ?></p>
 					<?php comments_template();
-				}
-				?>
-				<?php
-				?>
+				}?>
 			</div>
 		</div>
 	<?php endwhile; ?>		
 </div>
 <div class="pagebar"><p><?php posts_nav_link(); ?></p></div>
-<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
